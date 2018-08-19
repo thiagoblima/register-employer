7c0827417e9fa3865d6535bbf4b8fa5a2460a5fb
Thiago Lima <thitheguy@gmail.com>
2018-08-19 13:50:51 -0300

    Adding new instances of object for ann advanced standard employee

--- employee.h
+++ employee.h
@@ -16,18 +16,22 @@
 
 class employee {
 
-    // no need to set them as private,
-    // in C++ they're automatically private as default
-
+private:
     int empid;
+    int age;
+    char gender[1];
     char ename[10];
+    char elastname[10];
+    char email[20];
     float salary;
 
 public:
     void putEmp(int id, char name[10], float basic);
+    void putEmp(int id, int age, char gender[1], char ename[10], char elastname[10], char email[20], float salary);
     void getEmp();
+    void getCompleteEmp();
 
-}pemp;
+}pemp, aemp;
 
 void employee::putEmp(int id, char *name, float basic) {
     this->empid = id;
@@ -35,9 +39,29 @@ void employee::putEmp(int id, char *name, float basic) {
     this->salary = basic;
 }
 
+void employee::putEmp(int id, int age, char *gender, char *name, char *lastname, char *email, float basic) {
+    this->empid = id;
+    this->age = age;
+    strcpy(this->gender, gender);
+    strcpy(this->ename, name);
+    strcpy(this->elastname, lastname);
+    strcpy(this->email, email);
+    this->salary = basic;
+}
+
 void employee::getEmp() {
     printf("EmployeeId=%d\n", this->empid);
     printf("Employee Name=%s\n", this->ename);
+    printf("Employee Salary=%f\n",this->salary);
+}
+
+void employee::getCompleteEmp() {
+    printf("Employee Id=%d\n", this->empid);
+    printf("Employee Age=%d\n", this->age);
+    printf("Employee Gender=%s\n", this->gender);
+    printf("Employee Name=%s\n", this->ename);
+    printf("Employee Last Name=%s\n", this->elastname);
+    printf("Employee Email=%s\n", this->email);
     printf("Employee Salary=%f\n",this->salary);
 }
 
--- main.cpp
+++ main.cpp
@@ -3,10 +3,25 @@
 #include "conio.h"
 #include "employee.h"
 
+using namespace std;
+
 int main() {
     employee *e;
     e = &pemp;
+
+    cout << "Basic Standard Employee object model: " << endl << endl;
     e->putEmp(101, const_cast<char *>("f,d,a"), 22.789);
     e->getEmp();
+
+    cout << endl;
+
+    employee *a;
+    a = &aemp;
+
+    cout << "Complete Standard Employee object model: " << endl << endl;
+    a->putEmp(100, 23, const_cast<char *>("m"), const_cast<char *>("thiago"), const_cast<char *>("lima"),
+              const_cast<char *>("thiagolimasp@live.com"), 40.000);
+    a->getCompleteEmp();
+
     return 0;
 }
